bf91a93185baad9658dfe4dc8bf47e4d5ff6fc69
palash <palash@think42labs.com>
2019-08-21 17:28:52 +0530

    api endpoint

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,5 +5,5 @@ if(window.location.host === 'admin.altarjoy.com'){
 else if(window.location.host === 'bbstaging.club'){
     api = 'https://bg.bbstaging.club';
 }else{
-    api = 'https://b284d577.ngrok.io';
+    api = '3.15.171.10';
 }
