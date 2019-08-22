7f24c45a3b589ad55fa08e6d08f4f9922fceddf7
palash <palash@think42labs.com>
2019-08-22 17:39:31 +0530

    added file upload

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,5 +5,5 @@ if(window.location.host === 'admin.altarjoy.com'){
 else if(window.location.host === 'bbstaging.club'){
     api = 'https://bg.bbstaging.club';
 }else{
-    api = '3.15.171.10';
+    api = '3.15.171.10:8080';
 }
