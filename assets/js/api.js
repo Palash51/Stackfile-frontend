ec891a7ef9471de8fae66c01f40369eb1e637ebc
palash <palash@think42labs.com>
2019-08-23 11:21:28 +0530

    upload file api integrated

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,9 +1,9 @@
 var api;
-if(window.location.host === 'admin.altarjoy.com'){
-    api = 'https://bg.altarjoy.com';
+if(window.location.host === 'admin.com'){
+    api = 'https://admin.com';
 }
-else if(window.location.host === 'bbstaging.club'){
-    api = 'https://bg.bbstaging.club';
+else if(window.location.host === 'domaintest.club'){
+    api = 'https://bg.domain.club';
 }else{
-    api = '3.15.171.10:8080';
+    api = 'http://3.15.171.10:8080';
 }
--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,88 +1,28 @@
 $(document).ready(function(){
 
-	$('#singlebutton').on('click', function(){
+  $('#singlebutton').on('click', function(e){
+    e.preventDefault();
+    var data, xhr;
+    var token = localStorage.getItem('Token');
+    
+    data = new FormData();
+    data.append( 'file_url', $( '#filebutton' )[0].files[0] );
+    data.append('description', $('#description').val());
+    data.append('can_share', $('#checkbox').prop( "checked"));
+    xhr = new XMLHttpRequest();
+    
+    
+    xhr.open( 'POST', api+'/v1/file', true );
+    xhr.setRequestHeader('Authorization', 'Token '  + token);
+    
+    
+    xhr.onreadystatechange = function ( response ) {
+      if (this.status == 200) {
+            alert("You have successfully Uploaded File");
+        }
+    };
+    xhr.send(data);
 
-
-        var data, xhr;
-        var token = localStorage.getItem('Token');
-        data = new FormData();
-        data.append( 'file_url', $( '#filebutton' )[0].files[0] );
-        data.append('description', $('#description').val());
-        data.append('checkbox', $('#checkbox').prop( "checked"));
-        xhr = new XMLHttpRequest();
-        
-        // xhr.setRequestHeader('Authorization', 'Token '  + token);
-        xhr.open( 'POST', 'https://e502ed05.ngrok.io/v1/file', true );
-        xhr.setRequestHeader('Authorization', 'Token' + token);
-        
-        xhr.onreadystatechange = function ( response ) {};
-        xhr.send(data);
-
-        e.preventDefault();
-        
-        // var formData = new FormData($('#filebutton')[0]);
-        // const file = document.querySelector('.input-file');
-        // console.log(file);
-        // alert(file);
-
-        // var file_data = $('#filebutton').prop('files')[0];   
-        // var form_data = new FormData();                  
-        // form_data.append('file', file_data);
-        // console.log(file_data);
-        // alert(form_data); 
-
-        // var fd = new FormData();
-        // fd.append('fname', 'test.wav');
-        // fd.append('data', soundBlob);                      
-        
-        // $.ajax({
-        //     type: 'post',
-        //     url: api+'/v1/file',
-        //     cache: false,
-        //     contentType: false,
-        //     processData: false,
-        //     data: fd,
-        //     success: function (response) {
-        //         console.log(response);
-        //     },
-        //     error: function (err) {
-        //         console.log(err);
-        //     }
-        // });
-
-
-
-
-
-		// var description = $('#description').val();
-  //       var file_url = $('#filebutton').val();
-  //       var checkbox = $('#checkbox').prop( "checked");;
-        
-        
-  //       var data = {
-  //           description: description,
-  //           file_url: file_url,
-  //           can_share: checkbox
-  //       }
-        
-		// $.ajax({
-  //       url: api+'/v1/file',
-  //       method: 'POST',
-  //       headers: {
-  //           Authorization: 'Token ' + localStorage.getItem('Token')
-  //       },
-  //       contentType: 'application/json',
-  //       data: JSON.stringify(data),
-  //       async: false,
-  //       success: function(data){
-  //       	alert("Your file has been uploded Successfully");
-  //       },
-  //       error: function(err){
-  //           console.log(err);
-  //       	}
-  //   	});
-
-	})
-
-
-});
+    
+  })
+}); 
